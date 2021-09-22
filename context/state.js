555cb2df37b4f10ce7ad58c7ef78ca9b64636aa8
Daniel Watts <daniel.watts@bcgdv.com>
2021-09-22 11:36:24 +0100

    Add spinner loading state

--- context/state.js
+++ context/state.js
@@ -16,11 +16,15 @@ export function StateProvider({ children }) {
     const initialState = {
         routes: [],
         reverse: false,
+        loading: true,
         setRoutes: (routes) => {
             setState(previousState => ({...previousState, routes: routes}))
         },
         toggleReverse: () => {
             setState(previousState => ({...previousState, reverse: !previousState.reverse}))
+        },
+        isLoading: (loading) => {
+            setState(previousState => ({...previousState, loading: loading}))
         }
     };
 
@@ -30,6 +34,7 @@ export function StateProvider({ children }) {
         try {
             const res = await loadRoutes(false)
             state.setRoutes(res.data.routes)
+            state.isLoading(false)
         } catch (error) {
             console.log(error)
         }
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,18 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Route from '../components/route'
 import { useAppState, loadRoutes } from '../context/state'
-import { Switch } from 'antd';
+import { Switch, Spin } from 'antd';
 
 export default function Home({ }) {
   const state = useAppState();
 
   const toggleReverse = () => {
+    state.isLoading(true)
     loadRoutes(!state.reverse)
       .then((res) => {
         state.setRoutes(res.data.routes)
         state.toggleReverse()
+        state.isLoading(false)
       })
   }
 
@@ -25,13 +27,15 @@ export default function Home({ }) {
         <label>Reverse <Switch defaultChecked={state.reverse} onChange={toggleReverse} /></label>
       </div>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <ul className={utilStyles.list}>
-          {state.routes.map((route) => (
-            <li className={utilStyles.listItem} key={route.id}>
-              <Route route={route} />
-            </li>
-          ))}
-        </ul>
+        {state.loading 
+          ? <div className={utilStyles.spinner}><Spin size="large" /></div>
+          : <ul className={utilStyles.list}>
+              {state.routes.map((route) => (
+                <li className={utilStyles.listItem} key={route.id}>
+                  <Route route={route} />
+                </li>
+              ))}
+            </ul>}
       </section>
     </Layout>
   )
